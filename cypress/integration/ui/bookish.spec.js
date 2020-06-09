9a4cd204274c3c0472beee2f8892e9611f93f907
Hannah Bourke <hannah.bourke@thoughtworks.com>
2020-06-09 12:14:09 +1000

    Refactor mock server and test setup

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -1,17 +1,44 @@
-describe('Bookish application', function () {
-    it('Visits the bookish', function () {
-        cy.visit('http://localhost:3000/');
-        cy.get('h2[data-test="heading"]').contains('Bookish')
-    })
+import axios from "axios";
 
-    it('Shows a book list', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('div[data-test="book-list"]').should('exist');
-        cy.get('div.book-item').should((books) => {
-            expect(books).to.have.length(2);
-            
-            const titles = [...books].map(x => x.querySelector('h2').innerHTML);
-            expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
-        })
-    })
-})
+describe("Bookish application", () => {
+  before(() => {
+    return axios
+      .delete("http://localhost:8080/books?_cleanup=true")
+      .catch((err) => err);
+  });
+  beforeEach(() => {
+    const books = [
+        {"name": "Refactoring", "id": 1},
+        {"name": "Domain-driven design", "id": 2},
+        {"name": "Building Microservices", "id": 3}
+      ];
+
+    return books.map((item) =>
+      axios.post("http://localhost:8080/books", item, {
+        headers: { "Content-Type": "application/json" },
+      })
+    );
+  });
+
+  afterEach(() => {
+    return axios
+      .delete("http://localhost:8080/books?_cleanup=true")
+      .catch((err) => err);
+  });
+
+    it("Visits the bookish", function () {
+      cy.visit("http://localhost:3000/");
+      cy.get('h2[data-test="heading"]').contains("Bookish");
+    });
+
+  it("Shows a book list", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('div[data-test="book-list"]').should("exist");
+    cy.get('div.book-item').should((books) => {
+        expect(books).to.have.length(3);
+  
+        const titles = [...books].map(x => x.querySelector('h2').innerHTML);
+        expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design', 'Building Microservices'])
+      })
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
 import BookList from './BookList';
 
-function App() {
-  const books = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }];
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const res = await axios.get('http://localhost:8080/books');
+      setBooks(res.data);
+    };
+
+    fetchBooks();
+  }, []);
+  
   return (
     <div>
       <Typography variant="h2" component="h2" data-test="heading">
