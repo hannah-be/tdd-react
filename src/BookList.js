c241b926cbef80cda45b3f5bc73f6df11c3408a3
Hannah Bourke <hannah.bourke@thoughtworks.com>
2020-06-09 16:26:36 +1000

    chapter 4 refactor

--- src/BookList.js
+++ src/BookList.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const BookList = ({books}) => {
+const BookList = ({loading, error, books}) => {
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
+    if (error) {
+        return <p>Error...</p>
+    }
+
     return <div data-test="book-list">
         {
-            books.map(book => (<div className="book-item">
+            books.map(book => (<div className="book-item" key={book.id}>
                 <h2 className="title">{book.name}</h2>
             </div>))
         }
---
+++ src/BookListContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useRemoteService } from "./hooks";
+
+import BookList from "./BookList";
+
+const BookListContainer = () => {
+  const { data, loading, error } = useRemoteService([]);
+  return <BookList books={data} loading={loading} error={error}/>;
+};
+
+export default BookListContainer;
