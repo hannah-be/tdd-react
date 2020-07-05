4612ef35fcc7d7abf45f29ed53b45167a4bbbc96
Hannah Bourke <hannah.bourke@thoughtworks.com>
2020-07-05 12:11:31 +1000

    ch5 tests passing

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -8,10 +8,10 @@ describe("Bookish application", () => {
   });
   beforeEach(() => {
     const books = [
-        {"name": "Refactoring", "id": 1},
-        {"name": "Domain-driven design", "id": 2},
-        {"name": "Building Microservices", "id": 3}
-      ];
+      { name: "Refactoring", id: 1 },
+      { name: "Domain-driven design", id: 2 },
+      { name: "Building Microservices", id: 3 },
+    ];
 
     return books.map((item) =>
       axios.post("http://localhost:8080/books", item, {
@@ -26,26 +26,31 @@ describe("Bookish application", () => {
       .catch((err) => err);
   });
 
-    it("Visits the bookish", function () {
-      cy.visit("http://localhost:3000/");
-      cy.get('h2[data-test="heading"]').contains("Bookish");
-    });
+  it("Visits the bookish", function () {
+    cy.visit("http://localhost:3000/");
+    cy.get('h2[data-test="heading"]').contains("Bookish");
+  });
 
   it("Shows a book list", () => {
     cy.visit("http://localhost:3000/");
     cy.get('div[data-test="book-list"]').should("exist");
-    cy.get('div.book-item').should((books) => {
-        expect(books).to.have.length(3);
-  
-        const titles = [...books].map(x => x.querySelector('h2').innerHTML);
-        expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design', 'Building Microservices'])
-      })
+    cy.get("div.book-item").should((books) => {
+      expect(books).to.have.length(4);
+
+      const titles = [...books].map((x) => x.querySelector("h2").innerHTML);
+      expect(titles).to.deep.equal([
+        "Refactoring",
+        "Domain-driven design",
+        "Building Microservices",
+        "Acceptance Test Driven Development with React",
+      ]);
+    });
   });
 
-  it('Goes to the detail page', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('div.book-item').contains('View Details').eq(0).click();
-    cy.url().should('include', '/books/1');
-    cy.get('h2.book-title').contains('Refactoring');
+  it("Goes to the detail page", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("div.book-item").contains("View Details").eq(0).click();
+    cy.url().should("include", "/books/1");
+    cy.get("h2.book-title").contains("Refactoring");
   });
 });
